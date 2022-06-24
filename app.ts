f3a670eda507807cf28d413fee283b3c8de4c8e8
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-24 17:01:40 -0600

    Successfully entered campground data into MongoDB, we're in business baby!

--- app.ts
+++ app.ts
@@ -1,12 +1,32 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const Mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+Mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = Mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database successfully connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/make_campground', async (req, res) => {
+  const camp = new Campground({title: 'My Backyard', description: 'an awesome place with gardens and chickens!'});
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(33333, () => {
